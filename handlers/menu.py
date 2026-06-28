b24a7bb43d0677730570f41b18363188ac53ff93
javohir-dls <chorikichinov21@gmail.com>
2026-06-28 17:21:39 +0500

    Update menu.py

--- handlers/menu.py
+++ handlers/menu.py
@@ -1,1 +1,18 @@
+from telebot import types
 
+def register(bot):
+
+    @bot.callback_query_handler(func=lambda call: call.data == "check_sub")
+    def check_subscription(call):
+
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+        markup.row("🎵 MP3", "☁️ Ob-havo")
+        markup.row("🕌 Namoz", "💱 Valyuta")
+        markup.row("🌍 Dunyo soati", "👶 Yosh kalkulyator")
+
+        bot.send_message(
+            call.message.chat.id,
+            "✅ Asosiy menyu",
+            reply_markup=markup
+        )
