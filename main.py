721ea001c801097eaf22d4fae0730dccb96ecd0a
javohir-dls <chorikichinov21@gmail.com>
2026-06-28 17:16:39 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,12 @@
 import telebot
 from config import TOKEN
-
 from handlers import start
-from handlers import menu
-from handlers import mp3
-from handlers import weather
-from handlers import prayer
-from handlers import currency
-from handlers import time
 
 bot = telebot.TeleBot(TOKEN)
 
-# Handlerlarni ulash
+# Start handlerni ulash
 start.register(bot)
-menu.register(bot)
-mp3.register(bot)
-weather.register(bot)
-prayer.register(bot)
-currency.register(bot)
-time.register(bot)
 
-print("✅ JavoVerse ishga tushdi!")
+print("🚀 JavoVerse ishga tushdi!")
 
 bot.infinity_polling(skip_pending=True)
