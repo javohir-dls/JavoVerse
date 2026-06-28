bf7b97e2c099b59c397491780692b8ba84fa4614
javohir-dls <chorikichinov21@gmail.com>
2026-06-28 20:58:49 +0500

    Update currency.py

--- handlers/currency.py
+++ handlers/currency.py
@@ -1,34 +1,24 @@
+import requests
 from aiogram import Router, types
 
 router = Router()
 
-rates = {
-    "USD": 12500,
-    "EUR": 13500,
-    "RUB": 135,
-    "GBP": 16000,
-    "TRY": 400,
-    "KZT": 25,
-    "CNY": 1700,
-    "KRW": 9,
-    "JPY": 85,
-    "AED": 3400
-}
-
 @router.message(lambda m: "valyuta" in m.text.lower())
-async def currency_menu(message: types.Message):
-    text = "💱 Format:\nUSD 10\nEUR 5\nva hokazo"
-    await message.answer(text)
-
+async def currency_help(message: types.Message):
+    await message.answer("💱 Format: USD UZS 10")
 
 @router.message()
 async def convert(message: types.Message):
     try:
-        cur, amount = message.text.split()
+        from_cur, to_cur, amount = message.text.split()
         amount = float(amount)
 
-        if cur.upper() in rates:
-            result = amount * rates[cur.upper()]
-            await message.answer(f"💰 {amount} {cur.upper()} = {result} UZS")
+        url = f"https://api.exchangerate.host/convert?from={from_cur}&to={to_cur}&amount={amount}"
+        res = requests.get(url).json()
+
+        result = res["result"]
+
+        await message.answer(f"💰 Natija: {result} {to_cur}")
+
     except:
-        pass
+        await message.answer("❌ Format xato: USD UZS 10")
