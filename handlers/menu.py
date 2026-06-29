aadcd304cd364c10dfabd5a17f38357bbfa6fad6
javohir-dls <chorikichinov21@gmail.com>
2026-06-29 06:16:23 +0500

    Update menu.py

--- handlers/menu.py
+++ handlers/menu.py
@@ -1,16 +1,27 @@
+from aiogram import Router
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-def main_menu():
-    keyboard = [
-        [KeyboardButton(text="💱 Valyuta kursi")],
-        [KeyboardButton(text="🕒 Dunyo soati")],
-        [KeyboardButton(text="🕌 Namoz vaqti")],
-        [KeyboardButton(text="📿 Allohning 99 ismi")],
-        [KeyboardButton(text="🌤 Ob-havo")],
-        [KeyboardButton(text="🎂 Yosh kalkulyatori")]
-    ]
+router = Router()
 
-    return ReplyKeyboardMarkup(
-        keyboard=keyboard,
-        resize_keyboard=True
+
+def main_menu():
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=[
+            [
+                KeyboardButton(text="💱 Valyuta kursi"),
+                KeyboardButton(text="🕒 Dunyo soati")
+            ],
+            [
+                KeyboardButton(text="🕌 Namoz vaqti"),
+                KeyboardButton(text="📿 Allohning 99 ismi")
+            ],
+            [
+                KeyboardButton(text="🌦 Ob-havo"),
+                KeyboardButton(text="🎂 Yosh kalkulyatori")
+            ]
+        ],
+        resize_keyboard=True,
+        input_field_placeholder="Kerakli bo'limni tanlang..."
     )
+
+    return keyboard
