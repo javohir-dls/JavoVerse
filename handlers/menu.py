9392b2cafa94a99e517e59d0bb8b51f6e63813d9
javohir-dls <chorikichinov21@gmail.com>
2026-06-29 05:51:34 +0500

    Update menu.py

--- handlers/menu.py
+++ handlers/menu.py
@@ -1,1 +1,16 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+def main_menu():
+    keyboard = [
+        [KeyboardButton(text="💱 Valyuta kursi")],
+        [KeyboardButton(text="🕒 Dunyo soati")],
+        [KeyboardButton(text="🕌 Namoz vaqti")],
+        [KeyboardButton(text="📿 Allohning 99 ismi")],
+        [KeyboardButton(text="🌤 Ob-havo")],
+        [KeyboardButton(text="🎂 Yosh kalkulyatori")]
+    ]
+
+    return ReplyKeyboardMarkup(
+        keyboard=keyboard,
+        resize_keyboard=True
+    )
